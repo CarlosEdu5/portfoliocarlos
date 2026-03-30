e7fdceed21cd4c3533a78f085a4184bcc68766b9
Carlos <elcarloseduardo3@gmail.com>
2026-03-30 14:53:56 -0300

    interacao de elementos com css

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('a[href="projetos.html"]').addEventListener("click", function(e) {
-    e.preventDefault()
-
-    document.querySelector("#projetos").scrollIntoView({
-        behavior: "smooth"
-    })
-})
